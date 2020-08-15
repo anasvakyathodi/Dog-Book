d006f8a881a2125576a07d0bf85dcb1522ecadca
anasvakyathodi <anasvakyathodi@gmail.com>
2020-08-15 18:16:01 +0530

    Updates

---
+++ precache-manifest.153beb78eb4d66a435b50e65a49fc8f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f3b677fc2227570163b48dbe17d7d3",
+    "url": "/Dog-Book/index.html"
+  },
+  {
+    "revision": "49ad56427776967b1c2c",
+    "url": "/Dog-Book/static/js/2.313fc0f9.chunk.js"
+  },
+  {
+    "revision": "f01cf20f87878beb20bf2f939f8352f1",
+    "url": "/Dog-Book/static/js/2.313fc0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6e0027df6a559e069c7",
+    "url": "/Dog-Book/static/js/main.d6b737a7.chunk.js"
+  },
+  {
+    "revision": "4594d9b79f1755034027",
+    "url": "/Dog-Book/static/js/runtime-main.f03f2f2e.js"
+  }
+]);
