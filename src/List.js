e86a496ce2eef9d7a69b3a041e2010678743b602
anasvakyathodi <anasvakyathodi@gmail.com>
2020-10-09 06:29:20 +0530

    search field added

--- src/List.js
+++ src/List.js
@@ -14,9 +14,9 @@ import {
   Grid,
   Button,
   Divider,
-  Box
+  Box,
 } from "@material-ui/core";
-import AutoSearchComplete from './components/AutoSearchComplete';
+import AutoSearchComplete from "./components/AutoSearchComplete";
 import { ExpandMore as ExpandMoreIcon, Pets } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles({
@@ -29,9 +29,9 @@ const useStyles = makeStyles({
 });
 const MyList = (props) => {
   const classes = useStyles();
-  const { breedName, setBreedName,setValue } = props;
+  const { breedName, setBreedName, setValue } = props;
   const [breeds, setBreeds] = useState(undefined);
-  const [searchValue, setSearchValue] = useState(); 
+  const [searchValue, setSearchValue] = useState();
 
   const [expandedPanel, setExpandedPanel] = useState(false);
 
@@ -53,62 +53,81 @@ const MyList = (props) => {
 
   return (
     <>
-      <AutoSearchComplete searchValue={searchValue} setSearchValue={setSearchValue} breeds={breeds} handleChange={(e)=>handleChange(e)} />
+      <Grid container justify="flex-end">
+        <Grid item xs="12" md="4">
+          <AutoSearchComplete
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+            breeds={breeds}
+            handleChange={(e) => handleChange(e)}
+          />
+        </Grid>
+      </Grid>
       {breeds ? (
         Object.keys(breeds)
-        .filter(key => searchValue !== '' && searchValue !== undefined ? key === searchValue: 'null')
-        .map((key, i) => {
-          return (
-            <Accordion
-              key={i}
-              style={{ margin: "1rem" }}
-              onClick={() => {
-                setBreedName(key);
-              }}
-              expanded={expandedPanel === key} 
-              onChange={handleAccordionChange(key)}
-            >
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="key"
+          .filter((key) =>
+            searchValue !== "" && searchValue !== undefined
+              ? key === searchValue
+              : "null"
+          )
+          .map((key, i) => {
+            return (
+              <Accordion
+                key={i}
+                style={{ margin: "1rem" }}
+                onClick={() => {
+                  setBreedName(key);
+                }}
+                expanded={expandedPanel === key}
+                onChange={handleAccordionChange(key)}
               >
-                <Typography
-                  style={{ textTransform: "capitalize" }}
-                  className={
-                    breedName === key ? classes.Accordion_a : classes.Accordion
-                  }
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="key"
                 >
-                  {key}
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <List>
-                  {breeds[key].length === 0 ? (
-                    <Typography>No Sub-Breads</Typography>
-                  ) : (
-                    breeds[key].map((breed) => (
-                      <ListItem>
-                        <ListItemAvatar>
-                          <Avatar>
-                            <Pets />
-                          </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText primary={breed} />                       
-                      </ListItem>
-                    ))
-                  )}              
-                </List>
-              </AccordionDetails>
-              <Divider />
-              <Box p={2}>
-                  <Button variant="contained" color="secondary" onClick={() => setValue(1)} >
+                  <Typography
+                    style={{ textTransform: "capitalize" }}
+                    className={
+                      breedName === key
+                        ? classes.Accordion_a
+                        : classes.Accordion
+                    }
+                  >
+                    {key}
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <List>
+                    {breeds[key].length === 0 ? (
+                      <Typography>No Sub-Breads</Typography>
+                    ) : (
+                      breeds[key].map((breed) => (
+                        <ListItem>
+                          <ListItemAvatar>
+                            <Avatar>
+                              <Pets />
+                            </Avatar>
+                          </ListItemAvatar>
+                          <ListItemText primary={breed} />
+                        </ListItem>
+                      ))
+                    )}
+                  </List>
+                </AccordionDetails>
+                <Divider />
+                <Box p={2}>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => setValue(1)}
+                  >
                     Select
                   </Button>
-              </Box>
-            </Accordion>
-          );
-        })
+                </Box>
+              </Accordion>
+            );
+          })
       ) : (
         <Grid
           container
