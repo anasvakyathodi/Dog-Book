f907cf1c3019f491a248e5aa363ff5b8c4d34213
Ankit Negi <ankit.negi@calsoftinc.com>
2020-10-29 14:57:26 +0530

    Issue#11: Added Contributors to About section
    Fixed missing dependencies: react-promise-tracker, react-loader-spinner

--- src/About.js
+++ src/About.js
@@ -1,23 +1,47 @@
 import React from "react";
-import { Card, CardContent, Typography, Link } from "@material-ui/core";
+import { Card, CardContent, Typography, Link, CardHeader, makeStyles, Avatar } from "@material-ui/core";
+import * as data from './data/contributers-list.json';
+
+const useStyle = makeStyles(theme => ({
+  root: {
+      maxWidth: 600,
+      display: 'flex',
+      justifyContent: 'flex-start',
+      marginRight: theme.spacing(0),
+      marginBottom: theme.spacing(2),
+      borderRadius: 10
+  },
+}));
+
 const About = () => {
+  const classes = useStyle();
   return (
-    <Card>
-      <CardContent>
-        <Typography gutterBottom variant="body2" component="h2">
-          Developed By
-        </Typography>
-        <Typography variant="h3" component="p" gutterBottom>
-          Anas Vakyathodi
-        </Typography>
-        <Typography variant="subtitle1">Git :</Typography>
-        <Link href="https://github.com/anasvakyathodi">
-          <Typography variant="button" color="primary">
-            @anasvakyathodi
-          </Typography>
-        </Link>
-      </CardContent>
-    </Card>
+    <>
+      {
+        data.contributers.map((contributer) => (
+          <Card className={classes.root}>
+            <CardHeader
+            avatar={<Avatar alt="avatar" src={contributer.avatar_url}></Avatar>} 
+            title="Contributer"/>
+            <CardContent>
+              <Typography gutterBottom variant="body2" component="h2">
+                Developed By
+              </Typography>
+              <Typography variant="h3" component="p" gutterBottom>
+                {contributer.name}
+              </Typography>
+              <Typography variant="subtitle1">Git :</Typography>
+              <Link href={contributer.username}>
+                <Typography variant="button" color="primary">
+                  {contributer.username}
+                </Typography>
+              </Link>
+            </CardContent>
+          </Card>)
+        )
+      }
+    </>
+
   );
 };
 
